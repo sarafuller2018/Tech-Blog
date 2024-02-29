2b6d1a463065f4584b77fc1b4375481a6d607c97
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-02-28 18:28:31 -0600

    working on seed code

---
+++ seeds/blogPostData.js
@@ -0,0 +1,7 @@
+// imports blogpost model
+const { BlogPost } = require("../models");
+
+// creates array
+const blogpostdata = [
+    title: ""
+]
---
+++ seeds/userData.js
@@ -0,0 +1,32 @@
+// imports user model
+const { User } = require("../models");
+
+// creates array
+const userdata = [
+    {
+        username: "Sara123",
+        email: "sara123@hotmail.com",
+        password: "password12345"
+      },
+      {
+        username: "Ziggy123",
+        email: "ziggy123@hotmail.com",
+        password: "password12345"
+      },
+      {
+        username: "Kelsey123",
+        email: "kelsey123@hotmail.com",
+        password: "password12345"
+      },
+      {
+        username: "Avery123",
+        email: "avery123@hotmail.com",
+        password: "password12345"
+      },
+];
+
+// bulk creates seed data into a variable using the array
+const seedUser = () => User.bulkCreate(userdata);
+
+// exports to allow access
+module.exports = seedUser;
