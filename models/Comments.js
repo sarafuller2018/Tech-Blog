2987f6f52932449d975165e77ee8df60c1e3f8c3
Sara Fuller <149854566+sarafuller2018@users.noreply.github.com>
2024-03-03 14:55:29 -0600

    added comments model

---
+++ models/Comments.js
@@ -0,0 +1,48 @@
+// brings in built-in model from sequelize
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+// creates blogpost model
+class Comments extends Model {}
+
+Comments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        post_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+          blogpost_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'blogpost',
+              key: 'id',
+            },
+          },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comments',
+      }
+);
+
+// exports to allow access
+module.exports = Comments;
--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 // imports models
 const User = require("./User");
 const BlogPost = require("./BlogPost");
+const Comments = require("./Comments");
 
 // sets up relationships between models
 User.hasMany(BlogPost, {
@@ -9,6 +10,22 @@ User.hasMany(BlogPost, {
 
 BlogPost.belongsTo(User, {
     foreignKey: "user_id",
+});
+
+User.hasMany(Comments, {
+    foreignKey: "user_id",
+});
+
+Comments.belongsTo(User, {
+    foreignKey: "user_id",
+});
+
+BlogPost.hasMany(Comments, {
+    foreignKey: "blogpost_id",
+});
+
+Comments.belongsTo(BlogPost, {
+    foreignKey: "blogpost_id",
 });
 
 // exports to allow access
